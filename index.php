644875d93361e7d6808bc66060bfc94f9c1012df
greynovus <greynovus@mail.ru>
2022-12-15 22:16:52 +0200

    index_change

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ echo<<<html
 html;
 
 $ar = array(3,2,1,5,4);
+$ar_old = $ar;
 //echo $ar[1];
 echo "Было: <br/>";
 echo print_r($ar);
@@ -26,7 +27,16 @@ while ($isTrue){
         }
     }
 }
-echo print_r($ar);
+echo print_r($ar)."<br/><br/><br/><br/>";
+echo "Выведем итоговый массив в более удобном виде: <br/><br/> Было:<br/>";
+foreach ($ar_old as $element){
+    echo "[".$element."] ";
+}
+echo "<br/><br/>Сало:<br/>";
+foreach ($ar as $element){
+    echo "[".$element."] ";
+}
+
 echo
 <<<html
 <br/><br/>goodbye, my friend!
